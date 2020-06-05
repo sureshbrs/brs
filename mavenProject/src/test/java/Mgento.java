e9e3b63a411e9569cc3ea2a427a9e8e4e318737b
suri <suri@DESKTOP-EM25E1K>
2020-06-05 10:33:46 +0530

    second brs commit

--- mavenProject/src/test/java/Mgento.java
+++ mavenProject/src/test/java/Mgento.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
@@ -11,8 +12,8 @@ public class Mgento {
 	
 	@Test
 	public void positiveCredial()
-	{
-		WebDriver driver=new ChromeDriver();
+	{   
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
